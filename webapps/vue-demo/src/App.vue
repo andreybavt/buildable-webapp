b364df52e1e5a43196f300ce683c22f392f8c4de
aavtomonov <andrey.avtomonov@dataiku.com>
2020-04-30 12:45:50 +0200

    hackaiku vue component

--- webapps/vue-demo/src/App.vue
+++ webapps/vue-demo/src/App.vue
@@ -1,29 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>hi</h1>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--        <Hackaiku></Hackaiku>-->
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HelloWorld from './components/HelloWorld.vue'
+    import Hackaiku from "./components/Hackaiku";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            Hackaiku,
+            HelloWorld
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ webapps/vue-demo/src/components/Hackaiku.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="hello">
+        <img @click="hackaikuCount++" alt="Hackaiku logo" src="../assets/hackaiku.png" class="logo">
+
+        <h1>Hello hackaiku #<code class="count">{{hackaikuCount}}</code></h1>
+
+        <p>Buildable Webapps project by</p>
+        <div>
+            <img src="../assets/andrey.jpg" class="team">
+            <img src="../assets/lance.jpg" class="team">
+            <img src="../assets/thibault.jpg" class="team">
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Hackaiku',
+        data: () => {
+            return {
+                hackaikuCount: 2,
+            }
+        },
+        props: {
+            msg: String
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .team{
+        border-radius: 100%;
+        height: 80px;
+        padding: 20px;
+    }
+    .count {
+        font-size: 50px;
+        color: #12b2ad;
+    }
+    .logo {
+        height: 200px
+    }
+
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
